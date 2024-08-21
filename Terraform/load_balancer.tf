db88ca31ebc0cc6e6f2f44ccd5366021b610ea19
Jay <apple@apples-MacBook-Pro.local>
2024-08-21 14:48:59 +0530

    target group added

--- Terraform/load_balancer.tf
+++ Terraform/load_balancer.tf
@@ -7,22 +7,6 @@ resource "aws_lb" "app" {
   depends_on = [ aws_security_group.sg ]
 }
 
-resource "aws_lb_listener" "http-listener" {
-  load_balancer_arn = aws_lb.app.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTP"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
 resource "aws_lb_target_group" "target-group" {
   name     = "jenkins-task2-target"
   port     = 8080
@@ -37,6 +21,20 @@ resource "aws_lb_target_group_attachment" "jenkins-server" {
   depends_on = [ aws_instance.private ]
 }
 
+resource "aws_lb_target_group" "target-group-2" {
+  name     = "jenkins-task2-target-2"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "jenkins-server-2" {
+  target_group_arn = aws_lb_target_group.target-group.arn
+  target_id        = aws_instance.private.id
+  port             = 80
+  depends_on = [ aws_instance.private ]
+}
+
 resource "aws_lb_listener" "https-listener" {
   load_balancer_arn = aws_lb.app.arn
   port              = "443"
@@ -47,5 +45,17 @@ resource "aws_lb_listener" "https-listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target-group.arn
+  }
+}
+
+
+resource "aws_lb_listener" "http-listener" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.target-group-2.arn
   }
 }
